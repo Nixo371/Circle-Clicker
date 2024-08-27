252811ec435ec1c15864cac91159b7e8f8f3a0c8
achaseiiix <austin.c.coursey@gmail.com>
2024-08-26 21:03:59 -0500

    Add circle despawning

--- Assets/Scripts/CircleBehavior.cs
+++ Assets/Scripts/CircleBehavior.cs
@@ -7,10 +7,24 @@ using UnityEngine.Experimental.GlobalIllumination;
 public class CircleBehavior : MonoBehaviour, IPointerClickHandler, Item
 {
 	public Inventory inventory;
+	public float timer = 0;
+	public float maxTime = 3;
+	public float fadeTimeStart = 2;
+    private SpriteRenderer spriteRenderer;
 
-	private void Start()
+    private void Start()
 	{
 		addPhysics2DRaycaster();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+	private void Update()
+	{
+		// Increment timer irregardless of FPS
+		timer += Time.deltaTime;
+
+		checkExpired();
+		updateFade();
 	}
 
 	public int getItemID() { return 1; }
@@ -26,6 +40,33 @@ public class CircleBehavior : MonoBehaviour, IPointerClickHandler, Item
         if (physicsRaycaster == null)
         {
 			Camera.main.gameObject.AddComponent<Physics2DRaycaster>();
+        }
+    }
+
+	private void checkExpired()
+	{
+		if (timer > maxTime)
+		{
+			Object.Destroy(this.gameObject);
+		}
+	}
+
+	public void setTransparency(float newTransparency)
+	{
+        Color color = spriteRenderer.color;
+        color.a = newTransparency;
+        spriteRenderer.color = color;
+    }
+
+	private void updateFade()
+	{
+        if (timer > fadeTimeStart)
+        {
+            float timeSinceStart = timer - fadeTimeStart;
+            float fadeDuration = maxTime - fadeTimeStart;
+			// Transparency determined automatically based on how long the fade should be
+            float newTransparency = 1 - (timeSinceStart / fadeDuration);
+            setTransparency(newTransparency);
         }
     }
 }
